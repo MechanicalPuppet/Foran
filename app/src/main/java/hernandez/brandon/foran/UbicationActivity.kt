b3d9fc14703d89671f527ea0c4a395d69f65a16f
Werta <carlos.huerta013@gmail.com>
2021-04-12 01:28:11 -0700

    Modificacion en pantalla Ubicacion

--- app/src/main/java/hernandez/brandon/foran/UbicationActivity.kt
+++ app/src/main/java/hernandez/brandon/foran/UbicationActivity.kt
@@ -1,11 +1,26 @@
 package hernandez.brandon.foran
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class UbicationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ubication)
+
+        var foranApp: ImageView = findViewById(R.id.ForanImgUbication)
+        var backMenu: ImageView = findViewById(R.id.backMenuUbication)
+
+        foranApp.setOnClickListener {
+            var intent: Intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        backMenu.setOnClickListener {
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
