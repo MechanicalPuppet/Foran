3ee0307b504ed293507b311bad685406b011ceff
Werta <carlos.huerta013@gmail.com>
2021-04-12 19:24:23 -0700

    Pantalla ubicacion funcional

--- app/src/main/java/hernandez/brandon/foran/UbicationActivity.kt
+++ app/src/main/java/hernandez/brandon/foran/UbicationActivity.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class UbicationActivity : AppCompatActivity() {
+class UbicationActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ubication)
+        createFragment()
 
         var foranApp: ImageView = findViewById(R.id.ForanImgUbication)
         var backMenu: ImageView = findViewById(R.id.backMenuUbication)
@@ -22,5 +32,26 @@ class UbicationActivity : AppCompatActivity() {
             var intent: Intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun createFragment() {
+        val mapFragment  = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(27.484271, -109.959540)
+        var marker = MarkerOptions().position(coordinates).title("Soriana")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
     }
 }
