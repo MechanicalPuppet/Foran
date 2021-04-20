6b08f7700d8c8303aaf3ff72a0d06924da71f3be
Brandon Hernandez <56750116+MechanicalPuppet@users.noreply.github.com>
2021-04-20 11:24:14 -0700

    Integración de gradientes. Corregido botones eliminar cuenta.

--- app/src/main/java/hernandez/brandon/foran/DeleteProfileActivity.kt
+++ app/src/main/java/hernandez/brandon/foran/DeleteProfileActivity.kt
@@ -3,6 +3,7 @@ package hernandez.brandon.foran
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class DeleteProfileActivity : AppCompatActivity() {
@@ -11,10 +12,23 @@ class DeleteProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_delete_profile)
 
         var imgback: ImageView = findViewById(R.id.backMenu)
+        var btnEliminar: Button = findViewById(R.id.btnEliminarTuCuenta)
+        var btnCancelar: Button = findViewById(R.id.btnCancelarCuenta)
 
         imgback.setOnClickListener {
             var intent: Intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
+        }
+
+        btnEliminar.setOnClickListener {
+            var intent: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCancelar.setOnClickListener {
+            var intent: Intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
