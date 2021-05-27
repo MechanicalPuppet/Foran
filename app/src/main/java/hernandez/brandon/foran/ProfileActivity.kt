46e24149112b82c6446af2e4868f5889dcfdc2f6
cr-ls <carlos.huerta204666@potros.itson.edu.mx>
2021-05-26 18:35:30 -0700

    Subida de imagen de perfil a Firebase, falta persistencia

--- app/src/main/java/hernandez/brandon/foran/ProfileActivity.kt
+++ app/src/main/java/hernandez/brandon/foran/ProfileActivity.kt
@@ -1,20 +1,43 @@
 package hernandez.brandon.foran
 
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_profile.*
+import java.io.IOException
+import java.util.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var filePath: Uri? = null
+    private var PICK_IMAGE_REQUEST = 1234
+
+    internal var storage: FirebaseStorage? = null
+    internal var storageReference: StorageReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage!!.reference
+
+        btnElegirImagen.setOnClickListener(this)
+        btnGuardarPerfil.setOnClickListener(this)
+
         var imgback: ImageView = findViewById(R.id.backMenu)
         var btnCerrarSesion: Button = findViewById(R.id.btnCerrarSesion)
         var btnEliminarCuenta: Button = findViewById(R.id.btnEliminarCuenta)
-        /* Variable a la pantalla del Arturito */
 
         imgback.setOnClickListener {
             var intent: Intent = Intent(this, MenuActivity::class.java)
@@ -26,16 +49,60 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        btnEliminarCuenta.setOnClickListener{
+        btnEliminarCuenta.setOnClickListener {
             var intent: Intent = Intent(this, DeleteProfileActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        /**
-                 btnCerrarSesion.setOnClickListener {
-                 var intent: Intent = Intent(this, PantallaArturito::class.java)
-                 startActivity(intent)
-             }
-         */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            filePath = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                fotoPerfil!!.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v == btnElegirImagen)
+            showFileChooser()
+        else
+            uploadFile()
+    }
+
+    private fun uploadFile() {
+        if (filePath != null) {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Cargando...")
+            progressDialog.show()
+
+            val imageRef = storageReference!!.child("images/" + UUID.randomUUID().toString())
+            imageRef.putFile(filePath!!)
+                .addOnSuccessListener {
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "File Upload", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+                }
+                .addOnProgressListener { taskSnapShot ->
+                    val progress =
+                        100.0 * taskSnapShot.bytesTransferred / taskSnapShot.totalByteCount
+                    progressDialog.setMessage("Uploaded" + progress.toInt() + "%...")
+                }
+        }
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "SELECT PICTURE"), PICK_IMAGE_REQUEST)
     }
 }
